6b83293e61543d4c7700d50eedc57f5f720fac2a
Alexander <brunoalexandercasafranca@gmail.com>
2019-11-04 23:31:23 -0500

    Cambiando comillas simples por comillas dobles en archivo enviar.php

--- enviar.php
+++ enviar.php
@@ -1,12 +1,12 @@
 <?php
-    $destino = 'brunoalexander_dg@outlook.es';
-    $fullName = $_POST['fullname'];
-    $email = $_POST['email'];
-    $company = $_POST['company'];
-    $item = $_POST['item'];
-    $telephone = $_POST['telephone'];
-    $serviceList = $_POST['serviceList'];
-    $message = $_POST['message'];
+    $destino = "brunoalexander_dg@outlook.es";
+    $fullName = $_POST["fullname"];
+    $email = $_POST["email"];
+    $company = $_POST["company"];
+    $item = $_POST["item"];
+    $telephone = $_POST["telephone"];
+    $serviceList = $_POST["serviceList"];
+    $message = $_POST["message"];
 
     $contenido = "Nombre Completo: ".$fullname."\nE-mail: ".$email."\nEmpresa: ".$company."\nRubro: ".$item                ."\nTeléfono: ".$telephone."\nServicio: ".$serviceList."\nMensaje: ".$message;
     mail($destino, "Asunto", $contenido);
