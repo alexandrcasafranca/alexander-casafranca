ffe09ae1992f41c38031afac438281923e58bd3b
Alexander <brunoalexandercasafranca@gmail.com>
2019-11-04 23:03:11 -0500

    Agregando codigo php para el funcionamiento del formulario

---
+++ enviar.php
@@ -0,0 +1,20 @@
+<?php
+    $destino = 'brunoalexandercasafranca@gmail.com';
+    $fullName = $_POST['fullname'];
+    $email = $_POST['email'];
+    $company = $_POST['company'];
+    $item = $_POST['item'];
+    $telephone = $_POST['telephone'];
+    $serviceList = $_POST['serviceList'];
+    $message = $_POST['message'];
+
+    $contenido = "Nombre Completo: ".$fullname
+                ."\nE-mail: ".$email
+                ."\nEmpresa: ".$company
+                ."\nRubro: ".$item
+                ."\nTeléfono: ".$telephone
+                ."\nServicio: ".$serviceList
+                ."\nMensaje: ".$message;
+    mail($destino, "Asunto", $contenido);
+    header("Location:index.html");
+?>
